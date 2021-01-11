d4424c6fc2c11f396693ce6682257792ad4f2a87
Paweł Gajo <gpawel1997@gmail.com>
2021-01-11 18:52:31 +0100

    dodanie komponentu category-review

---
+++ src/app/post/components/category-review/category-review.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-category-review',
+  templateUrl: './category-review.component.html',
+  styleUrls: ['./category-review.component.css']
+})
+export class CategoryReviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
