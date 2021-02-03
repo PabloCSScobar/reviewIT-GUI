e4077353fc90c9d4e1690593bd069dd84dc1114b
Paweł Gajo <gpawel1997@gmail.com>
2021-02-03 20:47:53 +0100

    dodanie podstawowego routingu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PostDetailViewComponent } from './post/components/post-detail-view/post-detail-view.component';
+import { PostListComponent } from './post/components/post-list/post-list.component';
 import { ContainerComponent } from './post/pages/container/container.component';
 
 const routes: Routes = [
   {
-    path: '**',
-    component: ContainerComponent
-  }
+    path: '',
+    component: ContainerComponent,
+    children: [
+      {
+        path: 'list',
+        component: PostListComponent,
 
+      },
+      {
+        path: 'post',
+        component: PostDetailViewComponent,
+      },
+    ]
+  }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {enableTracing: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/post/post-routing.module.ts
+++ src/app/post/post-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PostDetailViewComponent } from './components/post-detail-view/post-detail-view.component';
+import { PostListComponent } from './components/post-list/post-list.component';
+import { ContainerComponent } from './pages/container/container.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    // {
+    //   path: '**',
+    //   component: PostListComponent,
+    //   outlet: 'sub'
+    // },
+    // {
+    //   path: 'post',
+    //   component: PostDetailViewComponent,
+    //   outlet: 'sub'
+    // },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
