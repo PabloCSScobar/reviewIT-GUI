62945fd7d6d81126f00518020f0eeb1ff09b8ff7
Paweł Gajo <gpawel1997@gmail.com>
2021-04-10 15:40:17 +0200

    stworzenie serwisu komponentu walidującego dane

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,18 +3,16 @@ import { CommonModule } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HttpClientModule } from '@angular/common/http';
 import { TimeAgoPipe } from './time-ago.pipe';
+import { ErrorMessageComponent } from './components/error-message/error-message.component';
 
 @NgModule({
-  declarations: [TimeAgoPipe],
-  imports: [
-    CommonModule,
-    FontAwesomeModule,
-    HttpClientModule
-  ],
+  declarations: [TimeAgoPipe, ErrorMessageComponent],
+  imports: [CommonModule, FontAwesomeModule, HttpClientModule],
   exports: [
     FontAwesomeModule,
     HttpClientModule,
-    TimeAgoPipe
-  ]
+    TimeAgoPipe,
+    ErrorMessageComponent,
+  ],
 })
-export class SharedModule { }
+export class SharedModule {}
