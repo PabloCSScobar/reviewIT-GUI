57121b3272fa385aaa9aa4704586a00aeeee73db
Paweł Gajo <gpawel1997@gmail.com>
2021-04-11 12:07:53 +0200

    dodanie new-answer do answer-list

--- src/app/post/components/answer-list/answer-list.component.ts
+++ src/app/post/components/answer-list/answer-list.component.ts
@@ -1,18 +1,16 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Answer } from '../../models/answer';
+import { Category } from '../../models/category';
 
 @Component({
   selector: 'app-answer-list',
   templateUrl: './answer-list.component.html',
-  styleUrls: ['./answer-list.component.css']
+  styleUrls: ['./answer-list.component.css'],
 })
 export class AnswerListComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
   @Input() answers: Answer[];
+  @Input() categories: Category[];
 
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
