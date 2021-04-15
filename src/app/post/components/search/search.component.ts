dba15e8a4a07404652ef53591f5d8672832f9a12
Paweł Gajo <gpawel1997@gmail.com>
2021-04-15 21:54:24 +0200

    edycja input search. zmiana ikony na material. Usunięcie modulu fontawesome

--- src/app/post/components/search/search.component.ts
+++ src/app/post/components/search/search.component.ts
@@ -1,16 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
 })
 export class SearchComponent implements OnInit {
-  searchIcon = faSearch;
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HttpClientModule } from '@angular/common/http';
 import { TimeAgoPipe } from './time-ago.pipe';
 import { ErrorMessageComponent } from './components/error-message/error-message.component';
 
 @NgModule({
   declarations: [TimeAgoPipe, ErrorMessageComponent],
-  imports: [CommonModule, FontAwesomeModule, HttpClientModule],
-  exports: [
-    FontAwesomeModule,
-    HttpClientModule,
-    TimeAgoPipe,
-    ErrorMessageComponent,
-  ],
+  imports: [CommonModule, HttpClientModule],
+  exports: [HttpClientModule, TimeAgoPipe, ErrorMessageComponent],
 })
 export class SharedModule {}
